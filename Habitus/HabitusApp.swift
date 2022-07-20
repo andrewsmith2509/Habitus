216a292fc1063bc5dc8500203226f9d654d55560
Andrew <andrewsmith2509@gmail.com>
2022-07-20 13:56:02 -0500

    Initial Commit

---
+++ Habitus/HabitusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitusApp.swift
+//  Habitus
+//
+//  Created by Andrew Smith on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
